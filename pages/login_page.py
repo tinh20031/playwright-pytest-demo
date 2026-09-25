ed0bc7c0fc54325925ad0da82538a49f48ad8893
tinh20031 <tinhtranquocx@gmail.com>
2026-09-25 08:54:30 +0700

    fix update login locators based on codege & delete workflow

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,24 +1,27 @@
+import re
 from playwright.sync_api import Page
+
+
 class LoginPage:
     URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+
     def __init__(self, page: Page):
         self.page = page
         self.username_input = page.get_by_role("textbox", name="Username")
-        self.password_input =  page.get_by_role("textbox", name="Password")
+        self.password_input = page.get_by_role("textbox", name="Password")
         self.login_button = page.get_by_role("button", name="Login")
+
+        # Locator alert lỗi sai thông tin lấy chính xác từ codegen
+        self.alert_invalid = page.get_by_role("alert").locator("div").filter(
+            has_text=re.compile(r"^Invalid credentials$"))
+
+        # Locator text cảnh báo bỏ trống
+        self.required_field = page.get_by_text("Required")
 
     def navigate(self):
         self.page.goto(self.URL)
 
-    def enter_username(self, username: str):
+    def login(self, username: str, password: str):
         self.username_input.fill(username)
-    def enter_password(self, password: str):
         self.password_input.fill(password)
-    def click_login(self):
         self.login_button.click()
-
-    def login(self , username: str, password: str):
-        self.enter_username(username)
-        self.enter_password(password)
-        self.click_login()
-
--- tests/test_datadriventesting.py
+++ tests/test_datadriventesting.py
@@ -1,7 +1,5 @@
 import pytest
 from playwright.sync_api import Page, expect
-from pytest_playwright.pytest_playwright import page
-
 from pages.login_page import LoginPage
 
 @pytest.mark.parametrize("username, password, is_success", [
@@ -10,19 +8,16 @@ from pages.login_page import LoginPage
     ("standard_user", "wrong_pass", False),
     ("", "secret_sauce", False),
 ])
-def test_login_page(page:Page, username, password, is_success):
+def test_login_page(page: Page, username, password, is_success):
     login_page = LoginPage(page)
     login_page.navigate()
     login_page.login(username, password)
 
     if is_success:
+        # Nếu đăng nhập thành công: chuyển sang trang dashboard
         expect(page).to_have_url("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")
     else:
-        expect.to_have_failure("Login failed")
-
-
-
-
-
-
-
+        # Nếu thất bại: kiểm tra xuất hiện alert "Invalid credentials" HOẶC thông báo "Required"
+        expect(login_page.alert_invalid.or_(login_page.required_field).first).to_be_visible()
+        # Đảm bảo URL vẫn ở trang login
+        expect(page).to_have_url(login_page.URL)
